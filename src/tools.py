cb6cc90057ff415c58ff3a13e6b65971cb347685
Lukasz Cieply <hurricane.pl.96@gmail.com>
2018-09-20 00:45:05 +0200

    added str_to_cords function

--- src/tools.py
+++ src/tools.py
@@ -79,22 +79,67 @@ def cords_list_to_str(list_of_white_pieces: list, list_of_black_pieces: list):
     for cord in list_of_pieces:
         x, y = cord
 
-        x = x*2
-        x = 15-x
-
         y = y*2
-        y = y+1
+        y = 15-y
 
-        line = empty_board[x]
+        x = x*2
+        x = x+1
+
+        line = empty_board[y]
         line = list(line)
 
         if cord in list_of_white_pieces:
-            line[y] = 'w'
+            line[x] = 'w'
         elif cord in list_of_black_pieces:
-            line[y] = 'b'
+            line[x] = 'b'
 
         line = "".join(line)
-        empty_board[x] = line
+        empty_board[y] = line
 
     return '\n'.join(empty_board)
 
+
+def str_to_cords(str_board: str):
+    """
+    Creates list od cords out of string visual representation.
+    Does opposite of :func:`cords_list_to_str`
+
+    :param str str_board: string with visual representation of board
+    :return two lists of pieces
+    :rtype: tuple
+    """
+
+    list_of_white_pieces = list()
+    list_of_black_pieces = list()
+
+    str_board = str_board.split("\n")
+    for y in range(16):
+        if y % 2 == 0:
+            continue
+        line = str_board[y]
+
+        y-=1
+        y/=2
+        y-=7
+        y = int(y)
+        y=abs(y)
+
+        line = list(line)
+
+        def remove_letter(letter):
+            ret_list = list()
+            while letter in line:
+                index = line.index(letter)
+                line[index] = ' '
+
+                index-=1
+                index/=2
+                index = int(index)
+
+                ret_list.append((index,y))
+            return ret_list
+
+        list_of_black_pieces.extend(remove_letter('b'))
+        list_of_white_pieces.extend(remove_letter('w'))
+
+    return list_of_white_pieces, list_of_black_pieces
